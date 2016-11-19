6028869b46c9706ac752b19338c566c9ab0f3eb0
Armin Hamar <arminmops@icloud.com>
2016-11-20 00:37:10 +0100

    Changed somethings in Window so it acts like an Singelton also addet support for some glfw functions (proabbably get removed later)

--- ptl/pb_window.cpp
+++ ptl/pb_window.cpp
@@ -4,50 +4,159 @@ namespace PB{
 
 	window::window()
 	{
-	  win =	glfwCreateWindow(880, 800, "", NULL, NULL);
+	   if(single)
+	   {
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		  win = glfwCreateWindow(880, 800, "", NULL, NULL);
+		  //Set The Window Max Swap interval (Framelock act _vsync)
+		  glfwSwapInterval(1);
+		  single = false;
+	   }
 	}
 
 	window::window(i16 size_y,i16 size_x, str title)
 	{
-		win = glfwCreateWindow(size_y,size_x,title.c_str(), NULL, NULL);
+	   if(single)
+	   {
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		  win = glfwCreateWindow(size_y,size_x,title.c_str(), NULL, NULL);
+		  //Set The Window Max Swap interval (Framelock act _vsync)
+		  glfwSwapInterval(1);
+		  single = false;
+	   }
 	}
 
 	window::window(r_i16 size_y,r_i16 size_x,str& title)
 	{
-		win = glfwCreateWindow(size_y,size_x,title.c_str(), NULL, NULL);
+	   if(single)
+	   {
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		  win = glfwCreateWindow(size_y,size_x,title.c_str(), NULL, NULL);
+		  //Set The Window Max Swap interval (Framelock act _vsync)
+		  glfwSwapInterval(1);
+		  single = false;
+	   }
 	}
 
 	window::window(str title)
 	{
-		win = glfwCreateWindow(800,800,title.c_str(), NULL, NULL);
+	   if(single)
+	   {
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		  win = glfwCreateWindow(800,800,title.c_str(), NULL, NULL);
+		  //Set The Window Max Swap interval (Framelock act _vsync)
+		  glfwSwapInterval(1);
+		  single = false;
+	   }
 	}
 
 	window::window(str & title)
 	{
-		 win = glfwCreateWindow(800,800,title.c_str(), NULL, NULL);
+	   if(single)
+	   {
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		  win = glfwCreateWindow(800,800,title.c_str(), NULL, NULL);
+		  //Set The Window Max Swap interval (Framelock act _vsync)
+		  glfwSwapInterval(1);
+		  single = false;
+	   }
 	}
 
 	window::window(window_discriptor wnd)
 	{
-		 win = glfwCreateWindow(static_cast<int>(wnd.size_x),static_cast<int>(wnd.size_y),wnd.title.c_str(), NULL, NULL);
+	   if(single)
+	   {
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		  win = glfwCreateWindow(static_cast<int>(wnd.size_x),static_cast<int>(wnd.size_y),wnd.title.c_str(), NULL, NULL);
+		  //Set The Window Max Swap interval (Framelock act _vsync)
+		  glfwSwapInterval(1);
+		  single = false;
+	   }
 	}
 
 	window::window(window_discriptor & wnd)
 	{
-		 win = glfwCreateWindow(wnd.size_x,wnd.size_y,wnd.title.c_str(), NULL, NULL);
+	   if(single)
+	   {
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+		  win = glfwCreateWindow(static_cast<int>(wnd.size_x),static_cast<int>(wnd.size_y),wnd.title.c_str(), NULL, NULL);
+		  //Set The Window Max Swap interval (Framelock act _vsync)
+		  glfwSwapInterval(1);
+		  single = false;
+	   }
 	}
+
+	bool  window::valid()
+	{
+	  if(!win)
+	  {
+		 return true;
+		 //Terminate GLFW if the window could not be Created
+		 glfwTerminate();
+		 std::cerr << "Could not Create Window" << '\n';
+	  }
+	  else
+	  {
+		 return false;
+	  }
+	}
+
+	void window::context()
+	{
+	  glfwMakeContextCurrent(win);
+	}
+
+	void  window::clear_color(color<i8> c)
+	{
+	  glClearColor(c.red,c.green,c.blue,1.0);
+	}
+
+	void  window::clear_color(color<f16> c)
+	{
+	  glClearColor(c.red,c.green,c.blue,1.0);
+	}
+
+	void  window::clear_color(color<f32> c)
+	{
+	  glClearColor(static_cast<float>(c.red),static_cast<float>(c.green),static_cast<float>(c.blue),1.0f);
+	}
+
+	void  window::clear_color(GLfloat r,GLfloat g,GLfloat b)
+	{
+	  glClearColor(r,g,b,1.0);
+	}
+
 
 	void window::clear()
 	{
+	  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 	}
 
 	//@NOTE: Waht was the Idea behind it ?
 	void window::swap()
 	{
+	  glfwSwapBuffers(win);
+	}
+
+	bool window::shouldclose()
+	{
+	  return !glfwWindowShouldClose(win);
 	}
 
 	window::~window()
 	{
+	  glfwDestroyWindow(win);
+	}
 
+	void window::events()
+	{
+	  glfwPollEvents();
 	}
 }
--- ptl/pb_window.hpp
+++ ptl/pb_window.hpp
@@ -6,29 +6,39 @@
 
 namespace PB {
 
-    struct window_discriptor
-    {
-        str title;
-        i64 size_x;
-        i64 size_y;
-    };
+	struct window_discriptor
+	{
+		str title;
+		i64 size_x;
+		i64 size_y;
+	};
 
-    class window
-    {
-    public:
-        window();
-        window(int size_y,int size_x,str title);
-        window(int& size_y, int& size_x, str& title);
-        window(str title);
-        window(str &title);
-        window(window_discriptor wnd);
-        window(window_discriptor &wnd);
-        void clear();
-        void swap();
-        ~window();
+	class window
+	{
+	public:
+		window();
+		window(int size_y,int size_x,str title);
+		window(int& size_y, int& size_x, str& title);
+		window(str title);
+		window(str &title);
+		window(window_discriptor wnd);
+		window(window_discriptor &wnd);
+		bool shouldclose();
 
-    private:
-        color<float> clear_color;
-        GLFWwindow *win;
-    };
+		//@NOTE: Clearing Functions sets the clear Color of the Window
+		void clear_color(color<i8> c);
+		void clear_color(color<f16> c);
+		void clear_color(color<f32> c);
+
+		void clear_color(GLfloat r,GLfloat g,GLfloat b);
+		void clear();
+		void swap();
+		bool valid();
+		void context();
+		void events();
+		~window();
+	private:
+		GLFWwindow *win;
+		bool single = true;
+	};
 }
